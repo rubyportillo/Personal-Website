43fc3b9c84c14e087667879830ae37c80783eab7
Ruby <138826814+rubyportillo@users.noreply.github.com>
2023-09-22 20:30:04 -0700

    Created new folder for style.css and style.scss

---
+++ script.js
@@ -0,0 +1,2 @@
+// Text for each section of the website
+var About = 'Proud LA Native, Banana Slug and Christian. My skillset ranges from web development to policy writing. During my professional career I have published an op-ed on housing affordability, published an OSINT teaching guide, and currently host my web development projects on GitHub. During my free time, you can find me going for walks on the beach, lifting weights and perfecting my Animal Crossing® empire.
