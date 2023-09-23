4b41db3f00fce68b0b1528a092439721d45a15e3
Ruby <138826814+rubyportillo@users.noreply.github.com>
2023-09-22 22:30:43 -0700

    Finalized style.css stylesheet and finalized navbar functionality

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+//Text for Headings
+function changeText(id){
+    if (id){
+        let text = [Home, About, Contact];
+        let links = ['Home', 'About', 'Contact'];
+    }
+}
+
+
+
 // Text for each section of the website
 var About = 'Proud LA Native, Banana Slug and Christian. My skillset ranges from web development to policy writing. During my professional career I have published an op-ed on housing affordability, published an OSINT teaching guide, and currently host my web development projects on GitHub. During my free time, you can find me going for walks on the beach, lifting weights and perfecting my Animal Crossing® empire.
